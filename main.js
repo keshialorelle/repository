3bfff4c06f4ac7c8f873869f00680657653605c6
keshialorelle <mbounelorelle@gmail.com>
2024-08-20 02:19:29 -0700

    first branch

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-const myImage = document.querySelector("img");
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox2.jpg") {
-    myImage.setAttribute("src", "images/firefox2jpg");
-  } else {
-    myImage.setAttribute("src", "images/firefox2jpg");
-  }
-};
+const myHeading = document.querySelector("h1");
+  myHeading.textContent ="Hello world!";
+
+
